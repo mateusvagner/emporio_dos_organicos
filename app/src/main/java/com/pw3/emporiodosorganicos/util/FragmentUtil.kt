de3773b58bcf55349912ede5c08ba9c9f8b87ff8
Mateus Vagner <mateus.vagner@gmail.com>
2023-05-28 16:10:25 -0300

    refactor: Improve snackbar

--- app/src/main/java/com/pw3/emporiodosorganicos/util/FragmentUtil.kt
+++ app/src/main/java/com/pw3/emporiodosorganicos/util/FragmentUtil.kt
@@ -2,9 +2,11 @@ package com.pw3.emporiodosorganicos.util
 
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
+import com.pw3.emporiodosorganicos.R
 
 fun Fragment.showSnackbar(message: String) {
     Snackbar
         .make(requireView(), message, Snackbar.LENGTH_LONG)
+        .setAnchorView(R.id.bottom_nav_view)
         .show()
 }
