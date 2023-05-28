23b851aae890096172eba813c0d2619f6f654438
Mateus Vagner <mateus.vagner@gmail.com>
2023-05-28 15:57:01 -0300

    refactor: update UiUtil

--- app/src/main/java/com/pw3/emporiodosorganicos/util/UiUtil.kt
+++ app/src/main/java/com/pw3/emporiodosorganicos/util/UiUtil.kt
@@ -1,18 +1,24 @@
 package com.pw3.emporiodosorganicos.util
 
+import android.content.Context
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
-import androidx.fragment.app.FragmentActivity
 import com.pw3.emporiodosorganicos.R
 
-fun showDialog(context: FragmentActivity, @StringRes title: Int, @StringRes message: Int, onPositiveClicked: () -> Unit) {
+fun showDialog(
+    context: Context,
+    @StringRes title: Int,
+    @StringRes message: Int,
+    @StringRes positiveText: Int,
+    onPositiveClicked: () -> Unit
+) {
     val alertDialog: AlertDialog = context.let {
         val builder = AlertDialog.Builder(it)
         builder.apply {
             setTitle(title)
             setMessage(message)
 
-            setPositiveButton(R.string.save) { _, _ ->
+            setPositiveButton(positiveText) { _, _ ->
                 onPositiveClicked()
             }
 
